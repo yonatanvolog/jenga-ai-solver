7fb03ad37e7c0f1fe3488f2fae0a4e29d6fb0761
yonatanvolog <yonatan.vologdin@gmail.com>
2024-08-17 20:26:32 +0300

    Added camera with screenshot taking
    Press S to take screenshot, currently saved at:
    Assets/Screenshots/

---
+++ Assets/Screenshot.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using System.IO;
+
+public class Screenshot : MonoBehaviour
+{
+    public Camera screenshotCamera; // Assign the camera from which you want to capture the screenshot in the Inspector
+    public int resolutionWidth = 128;  // Set the width of the screenshot in the Inspector
+    public int resolutionHeight = 64;  // Set the height of the screenshot in the Inspector
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S)) // Capture screenshot on 'S' key press
+        {
+            TakeScreenshot();
+        }
+    }
+
+    public void TakeScreenshot()
+    {
+        if (screenshotCamera == null)
+        {
+            Debug.LogError("Screenshot Camera is not assigned!");
+            return;
+        }
+
+        // Clear the screenshots folder before taking a new screenshot
+        ClearScreenshotsFolder();
+
+        string folderPath = "Assets/Screenshots/"; // The path of your project folder
+
+        if (!Directory.Exists(folderPath)) // If this path does not exist yet
+        {
+            Directory.CreateDirectory(folderPath); // It will get created
+        }
+
+        var screenshotName = 
+            "Screenshot_" + 
+            System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss") + // Puts the current time right into the screenshot name
+            ".png"; // Put your favorite data format here
+
+        // Render the camera's view to a RenderTexture
+        RenderTexture renderTexture = new RenderTexture(resolutionWidth, resolutionHeight, 24);
+        screenshotCamera.targetTexture = renderTexture;
+        RenderTexture.active = renderTexture;
+        screenshotCamera.Render();
+
+        // Create a Texture2D with the size of the RenderTexture
+        Texture2D texture = new Texture2D(resolutionWidth, resolutionHeight, TextureFormat.RGB24, false);
+        texture.ReadPixels(new Rect(0, 0, resolutionWidth, resolutionHeight), 0, 0);
+        texture.Apply();
+
+        // Crop the image based on the camera's viewport rect 'w' value
+        int croppedWidth = Mathf.RoundToInt(texture.width * screenshotCamera.rect.width);
+        Texture2D croppedTexture = new Texture2D(croppedWidth, texture.height);
+        Color[] pixels = texture.GetPixels(0, 0, croppedWidth, texture.height);
+        croppedTexture.SetPixels(pixels);
+        croppedTexture.Apply();
+
+        // Reset the camera's target texture
+        screenshotCamera.targetTexture = null;
+        RenderTexture.active = null;
+        Destroy(renderTexture);
+
+        // Save the cropped Texture2D as a PNG
+        string screenshotPath = Path.Combine(folderPath, screenshotName);
+        File.WriteAllBytes(screenshotPath, croppedTexture.EncodeToPNG());
+
+        Debug.Log("Screenshot saved: " + screenshotPath); // You get instant feedback in the console
+
+        // Optionally, destroy the textures to free memory
+        Destroy(texture);
+        Destroy(croppedTexture);
+    }
+
+    public void ClearScreenshotsFolder()
+    {
+        string folderPath = "Assets/Screenshots/";
+
+        if (Directory.Exists(folderPath))
+        {
+            string[] files = Directory.GetFiles(folderPath);
+            foreach (string file in files)
+            {
+                File.Delete(file);
+            }
+
+            Debug.Log("All previous screenshots deleted.");
+        }
+    }
+}
