66e1c042d438e915789dd2df6eb72c06f955def6
yonatanvolog <yonatan.vologdin@gmail.com>
2024-09-15 17:04:10 +0300

    Created new furniture scene
    copied the game over there + refactor

---
+++ Assets/Scripts/CommandDispatcher.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+
+public class CommandDispatcher : MonoBehaviour
+{
+    public PythonListener pythonListener;
+
+    // Enum for all the commands
+    public enum Command
+    {
+        StartGame,
+        // You can add more commands here as needed
+    }
+
+    void Start()
+    {
+        if (pythonListener == null)
+        {
+            pythonListener = GameObject.FindObjectOfType<PythonListener>();
+        }
+    }
+
+    // Method to send a command (generic)
+    public void SendCommand(Command command)
+    {
+        string commandString = ConvertCommandToString(command);
+        pythonListener.SendData(commandString);
+    }
+
+    // Method to send the StartGame command with player1 and player2 params
+    public void DispatchStartGame(int player1, int player2, int num_of_rounds)
+    {
+
+        string commandString = $"start {player1} {player2} {num_of_rounds}";
+        Debug.Log("sending to python: " + commandString);
+
+        pythonListener.SendData(commandString);
+    }
+    
+    public void DispatchFinishedMove(int level, string color)
+    {
+        string commandString = $"finished_move {level} {color}";
+        Debug.Log("sending to python: " + commandString);
+
+        pythonListener.SendData(commandString);
+    }
+    
+    public void DispatchEndGame()
+    {
+        string commandString = $"end_game";
+        Debug.Log("sending to python: " + commandString);
+
+        pythonListener.SendData(commandString);
+    }
+
+    // Method to handle converting enum to the appropriate command string
+    private string ConvertCommandToString(Command command)
+    {
+        switch (command)
+        {
+            case Command.StartGame:
+                return "StartGame";
+            // Handle other commands here
+            default:
+                throw new ArgumentException("Invalid command");
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,183 @@
+using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.UI;
+
+public enum PlayerType
+{
+    RANDOM,
+    DQN,
+    SARSA,
+    HUMAN
+}
+
+public class MainMenuManager : MonoBehaviour
+{
+    public TMP_Dropdown player1Dropdown;
+    public TMP_Dropdown player2Dropdown;
+    public TMP_Dropdown roundsDropdown; // Add dropdown for number of rounds
+    public Button startButton;
+    public Button quitButton;
+    public GameObject mainMenu; // Reference to the main menu
+
+    private PlayerType player1Selection;
+    private PlayerType player2Selection;
+    private int num_of_rounds; // To store selected number of rounds
+    public CameraController cameraController;
+    public bool menu_status = false;
+    public CommandDispatcher commandDispatcher;  // Add CommandDispatcher field
+    public GameObject gameInfo;
+    
+    void Start()
+    {
+        // Find CommandDispatcher in the scene
+        if (commandDispatcher == null)
+        {
+            commandDispatcher = GameObject.FindObjectOfType<CommandDispatcher>();
+        }
+        
+        // Find cameraController in the scene
+        if (cameraController == null)
+        {
+            cameraController = GameObject.FindObjectOfType<CameraController>();
+        }
+
+        // Set default value to "Human Player"
+        SetDefaultDropdownValue(player1Dropdown, "Human Player");
+        SetDefaultDropdownValue(player2Dropdown, "Human Player");
+
+        // Set default rounds value (optional, depending on your dropdown)
+        SetDefaultDropdownValue(roundsDropdown, "1"); // Example of default
+
+        // Add listeners for the buttons
+        startButton.onClick.AddListener(OnStartButtonPressed);
+        quitButton.onClick.AddListener(OnQuitButtonPressed);
+
+        // Add listeners for TMP dropdowns
+        player1Dropdown.onValueChanged.AddListener(delegate { DropdownValueChanged(player1Dropdown, 1); });
+        player2Dropdown.onValueChanged.AddListener(delegate { DropdownValueChanged(player2Dropdown, 2); });
+        roundsDropdown.onValueChanged.AddListener(delegate { RoundsDropdownValueChanged(roundsDropdown); });
+
+        // Set default selections to HUMAN
+        player1Selection = PlayerType.HUMAN;
+        player2Selection = PlayerType.HUMAN;
+        num_of_rounds = 1;
+    }
+
+    void Update()
+    {
+        // Enable the main menu if ESC is pressed
+        if (Input.GetKeyDown(KeyCode.Escape) && mainMenu.activeSelf == false)
+        {
+            commandDispatcher.DispatchEndGame();
+            ToggleMainMenu();
+        }
+
+        //Sometimes python misses 
+        if ((mainMenu.activeSelf == false && menu_status == true) || 
+            (mainMenu.activeSelf == true && menu_status == false))
+        {
+            ToggleMainMenu();
+        }
+    }
+
+    void DropdownValueChanged(TMP_Dropdown dropdown, int player)
+    {
+        PlayerType selectedType = GetPlayerTypeFromDropdown(dropdown.options[dropdown.value].text);
+
+        if (player == 1)
+        {
+            player1Selection = selectedType;
+        }
+        else if (player == 2)
+        {
+            player2Selection = selectedType;
+        }
+    }
+
+    void RoundsDropdownValueChanged(TMP_Dropdown dropdown)
+    {
+        string selectedRounds = dropdown.options[dropdown.value].text;
+        num_of_rounds = Int32.Parse(selectedRounds.Split(' ')[0]);
+    }
+
+    PlayerType GetPlayerTypeFromDropdown(string option)
+    {
+        switch (option)
+        {
+            case string s when s.StartsWith("Random"):
+                return PlayerType.RANDOM;
+
+            case string s when s.StartsWith("Deep"):
+                return PlayerType.DQN;
+
+            case string s when s.StartsWith("SARSA"):
+                return PlayerType.SARSA;
+
+            case string s when s.StartsWith("Human Player"):
+                return PlayerType.HUMAN;
+
+            default:
+                throw new ArgumentException("Unknown player type: " + option);
+        }
+    }
+
+
+    void SetDefaultDropdownValue(TMP_Dropdown dropdown, string defaultOption)
+    {
+        int defaultIndex = dropdown.options.FindIndex(option => option.text == defaultOption);
+        if (defaultIndex != -1)
+        {
+            dropdown.value = defaultIndex;
+            dropdown.RefreshShownValue(); // Refreshes the dropdown to show the correct label
+        }
+    }
+
+    void OnStartButtonPressed()
+    {
+        Debug.Log("Player 1: " + player1Selection);
+        Debug.Log("Player 2: " + player2Selection);
+        Debug.Log("Number of Rounds: " + num_of_rounds);
+
+        // Disable the main menu
+        ToggleMainMenu();
+
+        // Send "StartGame" command to Python via CommandDispatcher
+        StartGame(player1Selection, player2Selection, num_of_rounds);
+    }
+
+    void OnQuitButtonPressed()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+    void StartGame(PlayerType player1, PlayerType player2, int num_of_rounds)
+    {
+        // Logic to start the game with the chosen players and number of rounds
+        Debug.Log("Starting game with Player 1: " + player1 + " and Player 2: " + player2 + " and Rounds: " + num_of_rounds);
+        commandDispatcher.DispatchStartGame((int)player1, (int)player2, num_of_rounds);
+        // SceneManager.LoadScene("GameScene"); // Example of loading the game scene
+    }
+
+    // Method to toggle the main menu visibility
+    public void ToggleMainMenu()
+    {
+        if (mainMenu.activeSelf == true)
+        {
+            menu_status = false;
+            mainMenu.SetActive(false);
+            gameInfo.SetActive(true);
+            cameraController.ToggleMenuMode(false);
+            cameraController.ToggleMouseControl(true);
+        }
+        else
+        {
+            menu_status = true;
+            mainMenu.SetActive(true);
+            gameInfo.SetActive(false);
+            cameraController.ToggleMenuMode(true);
+            cameraController.ToggleMouseControl(false);
+        }
+    }
+}
